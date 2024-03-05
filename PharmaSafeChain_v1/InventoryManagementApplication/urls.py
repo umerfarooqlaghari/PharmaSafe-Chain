49f3a3e2ce04d146f4fbc60f8474b19ec664c94f
umerfarooqlaghari <mumerfarooqlaghari@gmail.com>
2024-03-05 15:25:08 +0500

    Adding Paths to Applications URLS.py

---
+++ PharmaSafeChain_v1/InventoryManagementApplication/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Add your URLs here when you're ready
+]
--- PharmaSafeChain_v1/PharmaSafeChain_v1/settings.py
+++ PharmaSafeChain_v1/PharmaSafeChain_v1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ioc(j5ek^)vh&-1*-kas&8#rf^h_9-7n!4h=r15!mg)suo$-wh'
+SECRET_KEY = 'django-insecure-ioc(j5ek^)vh&asdn,na-1*-kas&8#rf^h_9-7n!4h=r15!mg)suo$-wh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
